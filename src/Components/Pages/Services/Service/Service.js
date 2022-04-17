2eab8144c8b98d4df24c0a8828ed9d2c81a1facc
arburhan <buthan190629@outlook.com>
2022-04-17 19:49:43 +0600

    service card adde

--- src/Components/Pages/Services/Service/Service.js
+++ src/Components/Pages/Services/Service/Service.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import './Service.css';
 
-const Service = () => {
+const Service = (props) => {
+    const { name, image, description, price } = props.service;
+    const shoartDescription = description.slice(0, 45) + '...';
     return (
         <div>
+            <Card>
+                <Card.Img variant="top" className='img-fluid' style={{ height: '200px' }} src={image} />
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text title={description} >
+                        {description.length > 50 ? shoartDescription : description}
+                    </Card.Text>
+                    <Card.Text>
+                        Charge: ${price}
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer className="border-0 bg-transparent text-center">
+                    <button className="hire-button border-0">Hire Now</button>
+                </Card.Footer>
+            </Card>
 
         </div>
     );
--- src/Components/Pages/Services/Services.js
+++ src/Components/Pages/Services/Services.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from './Service/Service';
 
 const Services = () => {
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('services.josn')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
+    console.log(services);
+
     return (
-        <div>
+        <div className='container-fluid px-5' >
+            <h2>Services</h2>
+            <div className='row row-cols-1 row-cols-md-3 g-5' >
+                {
+                    services.map(service => <Service key={service.id} service={service}></Service>)
+                }
+            </div>
+
         </div>
     );
 };
