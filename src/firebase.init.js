0501720c82aebd46751f6d87321d3e9d723773ce
arburhan <buthan190629@outlook.com>
2022-04-17 20:11:24 +0600

    add firebase

---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAW2gN-2DjwRLOdtE6QNwfS7gxDvUAOumA",
+    authDomain: "ar-wild-grapher.firebaseapp.com",
+    projectId: "ar-wild-grapher",
+    storageBucket: "ar-wild-grapher.appspot.com",
+    messagingSenderId: "189285588857",
+    appId: "1:189285588857:web:dde5f5e9b12a57b402d9a8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
