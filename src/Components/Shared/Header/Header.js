e4bd97a772588a2b0b9b28563bb24ddf10b99a5a
arburhan <buthan190629@outlook.com>
2022-04-17 12:25:43 +0600

    active link added

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { Container, Nav, NavDropdown } from 'react-bootstrap';
+import { Container, Nav } from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar'
-import { Link } from 'react-router-dom';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 
 const Header = () => {
     return (
         <nav>
             <Navbar sticky='top' collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand as={Link} to="/home">AR Wild-Grapher</Navbar.Brand>
+                    <Navbar.Brand as={CustomLink} to="/home">AR Wild-Grapher</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link as={Link} to="/home">Home</Nav.Link>
-                            <Nav.Link as={Link} to="/pricing">Pricing</Nav.Link>
-                            <Nav.Link as={Link} to="/services">Services</Nav.Link>
-                            <Nav.Link as={Link} to="/ckeckout">Checkout</Nav.Link>
-                            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                            <Nav.Link as={CustomLink} to="/home">Home</Nav.Link>
+                            <Nav.Link as={CustomLink} to="/pricing">Pricing</Nav.Link>
+                            <Nav.Link as={CustomLink} to="/services">Services</Nav.Link>
+                            <Nav.Link as={CustomLink} to="/ckeckout">Checkout</Nav.Link>
+                            <Nav.Link as={CustomLink} to="/login">Login</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
@@ -25,5 +25,21 @@ const Header = () => {
         </nav>
     );
 };
+function CustomLink({ children, to, ...props }) {
+    let resolved = useResolvedPath(to);
+    let match = useMatch({ path: resolved.pathname, end: true });
+
+    return (
+        <div>
+            <Link
+                style={{ textDecoration: match ? "underline white" : "none" }}
+                to={to}
+                {...props}
+            >
+                {children}
+            </Link>
+        </div>
+    );
+}
 
 export default Header;
