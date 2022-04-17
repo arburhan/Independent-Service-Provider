bd642d6f89c76079d9eb35791c0c0e3c0159f19c
arburhan <buthan190629@outlook.com>
2022-04-17 16:43:17 +0600

    banner added

--- src/Components/Pages/Banner/Banner.js
+++ src/Components/Pages/Banner/Banner.js
@@ -5,8 +5,11 @@ import './Banner.css';
 const Banner = () => {
     return (
         <div className='banner' >
-
-
+            <div className='banner-text mx-auto' >
+                <h1>AR Wild-Grapher</h1>
+                <p className='fs-4' >A Professional Wildlife Photographer</p>
+                <button className='button-more'>Learn More</button>
+            </div>
         </div>
     );
 };
