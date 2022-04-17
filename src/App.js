89f719a60999f3cab229307893f46cebaf88916e
arburhan <buthan190629@outlook.com>
2022-04-17 12:53:41 +0600

    log & register toggle done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './Components/Pages/Login/Login';
+import SignUp from './Components/Pages/SignUp/SignUp';
 import Footer from './Components/Shared/Footer/Footer';
 import Header from './Components/Shared/Header/Header';
 
@@ -9,7 +10,8 @@ function App() {
     <div>
       <Header></Header>
       <Routes>
-        <Route path='login' element={<Login></Login>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<SignUp></SignUp>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
--- src/Components/Pages/SignUp/SignUp.js
+++ src/Components/Pages/SignUp/SignUp.js
@@ -1,38 +1,40 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import { useLocation, useNavigate } from 'react-router-dom';
-import './Login.css';
 
-const Login = () => {
+const SignUp = () => {
     const navigate = useNavigate();
     const location = useLocation();
-    let from = location.state?.from?.pathname || '/';
-    const redirectRegister = () => {
-        navigate('/register' + location.search);
-    }
+    const redirectLogin = () => {
+        navigate('/login' + location.search);
 
+    }
     return (
         <div className='container-fluid w-75 mx-auto pt-5'>
             <Form className='text-center'>
                 <div className='' >
+                    <label className='d-block text-start' htmlFor="name">Name</label>
+                    <input type="name" name="name" id="name" required />
+                </div>
+                <div className='py-3' >
                     <label className='d-block text-start' htmlFor="email">Email</label>
                     <input type="email" name="email" id="email" required />
                 </div>
-                <div className='py-3' >
+                <div className='' >
                     <label className='d-block text-start' htmlFor="password">Password</label>
                     <input type="password" name="password" id="password" required />
                 </div>
             </Form>
             <div className='text-center'>
-                <div>
-                    New on AR Wild-Grapher? <button onClick={redirectRegister} className='signup-redirect'>Please Register</button>
+                <div className='py-2'>
+                    Already have an account? <button onClick={redirectLogin} className='signup-redirect'>Please Login</button>
                 </div>
                 <div className=''>
-                    <button className='btn submit-btn'>Login</button>
+                    <button className='btn submit-btn'>Register</button>
                 </div>
             </div>
         </div>
     );
 };
 
-export default Login;
+export default SignUp;
