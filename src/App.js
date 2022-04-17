5e18de87fcf75b17ebf6afa9cd7a0f2a93f84f78
arburhan <buthan190629@outlook.com>
2022-04-17 15:41:05 +0600

    add banner image

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Blogs from './Components/Pages/Blogs/Blogs';
 import Contact from './Components/Pages/Contact/Contact';
+import Home from './Components/Pages/Home/Home';
 import Login from './Components/Pages/Login/Login';
 import Services from './Components/Pages/Services/Services';
 import SignUp from './Components/Pages/SignUp/SignUp';
@@ -13,6 +14,8 @@ function App() {
     <div>
       <Header></Header>
       <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<SignUp></SignUp>}></Route>
         <Route path='/services' element={<Services></Services>}></Route>
---
+++ src/Components/Pages/Banner/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import banner from '../../../images/wildlifebanner.jpg';
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <div className='banner' >
+
+
+        </div>
+    );
+};
+
+export default Banner;
