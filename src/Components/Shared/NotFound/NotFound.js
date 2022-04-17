95d39ca3eff89d82f22f60dddb82e5c72ca698fb
arburhan <buthan190629@outlook.com>
2022-04-17 23:21:44 +0600

    style added on 404

--- src/Components/Shared/NotFound/NotFound.js
+++ src/Components/Shared/NotFound/NotFound.js
@@ -3,10 +3,10 @@ import image from '../../../images/notfound.webp';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>404 Not Found</h2>
+        <div className='text-center'>
+            <h2 className='text-danger'>404 Not Page Found</h2>
             <div>
-                <img src={image} alt="" />
+                <img className='img-fluid' style={{ height: '90vh' }} src={image} alt="" />
             </div>
         </div>
     );
