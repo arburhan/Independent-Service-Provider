1dc38ca05c1d57d341a325cb432b2e037377e8b6
arburhan <buthan190629@outlook.com>
2022-04-17 11:01:21 +0600

    set route & work on navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './Components/Shared/Footer/Footer';
+import Header from './Components/Shared/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header></Header>
+      <Routes>
 
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
