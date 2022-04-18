924ed62d9aecff9a559fe15b5f932d2055947667
arburhan <buthan190629@outlook.com>
2022-04-18 22:57:06 +0600

    customize readme file

--- src/Components/Pages/Checkout/Checkout.js
+++ src/Components/Pages/Checkout/Checkout.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Checkout = () => {
     return (
-        <div>
-            <h2>Checkout</h2>
+        <div className='text-center'>
+            <h2 className='py-5'>Checkout</h2>
+            <h2 className='pb-5'>Thanks for vising</h2>
 
         </div>
     );
