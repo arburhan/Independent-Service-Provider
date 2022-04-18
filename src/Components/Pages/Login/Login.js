b5a13766088d3b2cf19d9692df855b83e955aaa7
arburhan <buthan190629@outlook.com>
2022-04-18 11:55:18 +0600

    login with email done

--- src/Components/Pages/Login/Login.js
+++ src/Components/Pages/Login/Login.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Form from 'react-bootstrap/Form';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import './Login.css';
 
 const Login = () => {
@@ -10,25 +12,40 @@ const Login = () => {
     const redirectRegister = () => {
         navigate('/register' + location.search);
     }
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    const handleLogIn = e => {
+        e.preventDefault();
+        const userEmail = emailRef.current.value;
+        const userPassowrd = passwordRef.current.value;
+        signInWithEmailAndPassword(userEmail, userPassowrd);
+        navigate('/home');
+    }
 
     return (
         <div className='container-fluid w-75 mx-auto pt-5'>
-            <Form className='text-center'>
+            <Form onSubmit={handleLogIn} className='text-center'>
                 <div className='' >
                     <label className='d-block text-start' htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" required />
+                    <input ref={emailRef} type="email" name="email" id="email" required />
                 </div>
                 <div className='py-3' >
                     <label className='d-block text-start' htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" required />
-                </div>
-            </Form>
-            <div className='text-center'>
-                <div>
-                    New on AR Wild-Grapher? <button onClick={redirectRegister} className='signup-redirect'>Please Register</button>
+                    <input ref={passwordRef} type="password" name="password" id="password" required />
                 </div>
                 <div className=''>
-                    <button className='btn submit-btn'>Login</button>
+                    <button type='submit' className='btn submit-btn'>Login</button>
+                </div>
+            </Form>
+            <div className='text-center py-3'>
+                <div>
+                    New on AR Wild-Grapher? <button onClick={redirectRegister} className='signup-redirect'>Please Register</button>
                 </div>
             </div>
         </div>
