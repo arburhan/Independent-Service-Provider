5dea3beff1c5589c7f48ff6c74e58b0eceb2b2d4
arburhan <buthan190629@outlook.com>
2022-04-18 16:54:27 +0600

    google sign done

--- src/Components/Shared/SocialSign/SocialSign.js
+++ src/Components/Shared/SocialSign/SocialSign.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
+import Loading from '../Loading/Loading';
 import './SocialSign.css';
 
 const SocialSign = () => {
+    const navigate = useNavigate();
     const [signInWithGoogle, googleUser, googleLoading, googlError] = useSignInWithGoogle(auth);
+    const googleSign = () => {
+        signInWithGoogle();
+    }
+    let errors;
+
+    useEffect(() => {
+        if (googlError) {
+            errors = googlError.message;
+        }
+
+    }, [googlError])
+    useEffect(() => {
+        if (googleUser) {
+            navigate('/');
+        }
+
+    }, [googleUser])
+    if (googleLoading) {
+        return <Loading></Loading>;
+    }
 
     return (
         <div className='container my-4'>
@@ -13,10 +36,10 @@ const SocialSign = () => {
                 <p className='mt-2 px-3 fs-5'>or</p>
                 <hr className='w-25' />
             </div>
+            <p className='text-danger text-center'>{errors}</p>
             <div className='text-center'>
-                <button className='google-btn rounded-pill px-5 py-3 text-light' >Continue with google</button>
+                <button onClick={googleSign} className='google-btn rounded-pill px-5 py-3 text-light' >Continue with google</button>
             </div>
-
         </div>
     );
 };
