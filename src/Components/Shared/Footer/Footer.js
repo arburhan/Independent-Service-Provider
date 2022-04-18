00297484285ca53f8fb42e158b5103366381ad3a
arburhan <buthan190629@outlook.com>
2022-04-18 19:44:52 +0600

    footer added

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const Footer = () => {
+    let date = new Date().getFullYear();
     return (
-        <div>
-
+        <div className="container-fluid bg-dark py-4">
+            <div className='text-center text-light' >
+                Copyrignt &copy; {date} AR Wild-Grapher, all rights reserved.
+            </div>
         </div>
+
     );
 };
 
